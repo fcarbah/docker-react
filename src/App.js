592acf76296770d3c3893feb5411b8479d1985fc
Francis Carbah <fcarbah@gmail.com>
2019-02-04 16:06:49 -0500

    updated text on home page

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React JS- New Lessons!
           </a>
         </header>
       </div>
